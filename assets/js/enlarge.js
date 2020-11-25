c2cbeaa4f302e3c17d49c56364813a34c7fee691
wenzstev <wenzelstev@gmail.com>
2020-11-25 17:43:56 -0600

    adjusted modals and other css changes

--- assets/js/enlarge.js
+++ assets/js/enlarge.js
@@ -8,12 +8,14 @@ const enlarge = (element) => {
   console.log(element)
   var clone = element.cloneNode()
   modal.style.visibility = "visible"
-  backdrop.style.visibility="visible"
+  backdrop.style.visibility = "visible"
+  backdrop.style.opacity = "1"
   modal.appendChild(clone)
 }
 
 const closeModal = () => {
   modal.innerHTML = ''
   modal.style.visibility="hidden"
-  backdrop.style.visibility="hidden"
+  backdrop.style.opacity = "0";
+  backdrop.style.visibility="hidden";
 }
